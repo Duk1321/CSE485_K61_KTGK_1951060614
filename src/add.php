19a542e36c8afea72578ed58412a72cbc28d2e1d
duk1321 <duckest100#@gmail.com>
2021-10-22 10:53:18 +0700

    thiết kế chức năng add

--- src/add.php
+++ src/add.php
@@ -7,7 +7,7 @@
     <h1 class="text-center bg-dark text-light">THÊM BÀI</h1>
 </div>
 <div class="container pt-5">
-    <form action="" method="POST">
+    <form action="process_add.php" method="POST">
         <div class="mb-3">
             <label class="form-label">Tên bài thi</label>
             <input type="text" class="form-control" id="txtName" name="txtExamName">
@@ -30,17 +30,7 @@
         </div>
         <div class="mb-3">
             <label class="form-label">Trạng thái</label>
-            <div class="dropdown">
-                <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2"
-                    data-bs-toggle="dropdown" aria-expanded="false">
-                    Dropdown
-                </button>
-                <ul class="dropdown-menu" aria-labelledby="dropdownMenu2">
-                    <li><button class="dropdown-item" type="button">Action</button></li>
-                    <li><button class="dropdown-item" type="button">Another action</button></li>
-                    <li><button class="dropdown-item" type="button">Something else here</button></li>
-                </ul>
-            </div>
+            <input type="text" class="form-control" id="txtStatus" name="txtStatus">
         </div>
         <div class="mb-3">
             <label class="form-label">Mã bài thi</label>
---
+++ src/process_add.php
@@ -0,0 +1,21 @@
+<?php
+    $exam_title = $_POST['txtExamName'];
+    $exam_date = $_POST['txtE_Date'];
+    $time = $_POST['txtDuration'];
+    $question = $_POST['txtQuestion'];
+    $score = $_POST['txtScore'];
+    $status = $_POST['txtStatus'];
+    $exam_code = $_POST['ExamCode'];
+    $date = date("Y/m/d");
+    
+    include '../reuse/connect.php';
+
+    $sql = "INSERT INTO `exams`(`exam_title`, `exam_datetime`, `duration`, `total_question`, `marks_per_right_answer`, `created_on`, `status`, `exam_code`) 
+    VALUES ('$exam_title','$exam_date','$time','$question','$score', '$date', '$status', '$exam_code')";
+
+    $result = mysqli_query($conn,$sql);
+
+    if($result >0){
+        header("Location:../index.php");
+    }
+?>
